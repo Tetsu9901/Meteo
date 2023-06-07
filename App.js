1bac175f32f3adc8ccb2c18bd6f62b0e3f4bb69c
Tetsu9901 <t.marion@lyon.ort.asso.fr>
2023-06-07 15:30:40 +0200

    Fix dynamique location

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Meteo from './components/meteo';
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      console.log(status);
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+    })();
+  }, []);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      {latitude && longitude && <Meteo lat={latitude} long={longitude} />}
+      {errorMsg && <Text>{errorMsg}</Text>}
     </View>
   );
 }
@@ -16,5 +41,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'lightyellow'
   },
 });
---
+++ components/meteo.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, Image } from "react-native";
+
+const APIkey = "0552deb391a2bfe2a6306d2c085a9ac8"; 
+
+const Meteo = ({lat, long}) => {
+
+  const [meteoData, setMeteoData] = useState(null);
+  const [meteoIcon, setMeteoIcon] = useState(null);
+
+
+  useEffect(() => {
+    const fetchMeteoData = () => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&yon&units=metric&lang=fr&appid=${APIkey}`
+      )
+        .then(response => response.json())
+        .then(data => {
+          setMeteoData(data);
+          setMeteoIcon(`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+        })
+        .catch(error => console.error(error));
+    };
+
+    fetchMeteoData();
+  }, []);
+
+  return (
+  <View>
+    {meteoData ? (
+      <View>
+        <Text>Vous etes à : {meteoData.name}</Text>
+        <Text>{meteoData.main?.temp} °C</Text>
+        {meteoIcon ? 
+          <Image source={{ uri: meteoIcon}} style={{ width: 55, height: 55}} /> : null
+        }
+        <Text>{meteoData.weather[0].description}</Text>
+      </View>
+    ) : (
+      <Text>Loading...</Text>
+    )}
+  </View>
+  );
+};
+
+
+export default Meteo;
